5125a31fe08f3be778c9c18936001e74973dd09d
NiiKoloo <51046727+MaanKoloo@users.noreply.github.com>
2022-05-31 20:05:14 -0400

    Add files via upload

---
+++ assets/js/time.js
@@ -0,0 +1,29 @@
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('time').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+$(document).on({
+    "contextmenu": function(e) {
+        console.log("ctx menu button:", e.which); 
+
+        e.preventDefault();
+    },
+    "mousedown": function(e) { 
+        console.log("normal mouse down:", e.which); 
+    },
+    "mouseup": function(e) { 
+        console.log("normal mouse up:", e.which); 
+    }
+});
